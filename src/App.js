13f034a01f1a1d96ba4de3f52ea21a9b05864b1e
a.kuznecov <a.kuznecov@uremont.com>
2018-08-24 19:17:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello world!!</h1>
+        </header>
+          <div className="fruit-list">
+              <h1>Fruit List for {this.props.name}</h1>
+              <ul>
+                  <li>Апельсин</li>
+                  <li>Банан</li>
+                  <li>Киви</li>
+              </ul>
+          </div>
+      </div>
+    );
+  }
+}
+
+class FruitList extends Component {
+    render() {
+        return (
+            <div className="fruit-list">
+                <h1>Fruit List for {this.props.name}</h1>
+                <ul>
+                    <li>Апельсин</li>
+                    <li>Банан</li>
+                    <li>Киви</li>
+                </ul>
+            </div>
+        );
+    }
+}
+export default App;
