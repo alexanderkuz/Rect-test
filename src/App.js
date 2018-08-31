a2865601bec2e93b990e39c50923a6e1f055c8b6
a.kuznecov <a.kuznecov@uremont.com>
2018-08-31 18:52:17 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,121 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world!!</h1>
-        </header>
-          <div className="fruit-list">
-              <h1>Fruit List for {this.props.name}</h1>
-              <ul>
-                  <li>Апельсин</li>
-                  <li>Банан</li>
-                  <li>Киви</li>
-              </ul>
-          </div>
-      </div>
-    );
-  }
+
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
 }
 
-class FruitList extends Component {
+const element2 = <div tabIndex="0">dksdlks;l</div>;
+
+const element3 = (
+    <h1 className="greeting">
+        Hello, world!
+    </h1>
+);
+
+
+
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+};
+
+
+const element4 = <Welcome name="Sara" />;
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+};
+function App2() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+             src={props.user.avatarUrl}
+             alt={props.user.name}/>
+    );
+};
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+};
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+};
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+};
+
+class App extends Component {
+
+
+
+    handleClick = () => {
+        import('./moduleA')
+            .then(({ moduleA }) => {
+                // Use moduleA
+            })
+            .catch(err => {
+                // Handle failure
+            });
+    };
+
     render() {
         return (
-            <div className="fruit-list">
-                <h1>Fruit List for {this.props.name}</h1>
-                <ul>
-                    <li>Апельсин</li>
-                    <li>Банан</li>
-                    <li>Киви</li>
-                </ul>
-            </div>
+            <Comment
+                date={comment.date}
+                text={comment.text}
+                author={comment.author} />
         );
     }
 }
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const INTERVAL = 100;
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 0};
+    }
+
+    increment(){
+        this.setState({value: this.state.value + 1});
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.increment(), 1000/INTERVAL);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    render() {
+        const value = this.state.value
+        return (
+            <div>
+                <p>Таймер:</p>
+                <p>
+                    <span>{Math.round(value/INTERVAL/60/60)} : </span>
+                    <span>{Math.round(value/INTERVAL/60)} : </span>
+                    <span>{Math.round(value/INTERVAL)} . </span>
+                    <span>{value % INTERVAL}</span>
+                </p>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<Timer/>, document.getElementById('root2'));
